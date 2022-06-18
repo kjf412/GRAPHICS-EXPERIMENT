3621a7a00c32cb6bd553e50fa43a2348cadccf8c
kjf <87750093+kjf412@users.noreply.github.com>
2022-06-18 10:46:37 +0800

    Update KongJiFan_20201060295_work1.cpp

--- 实验一/KongJiFan_20201060295_work1.cpp
+++ 实验一/KongJiFan_20201060295_work1.cpp
@@ -1,1 +1,35 @@
+#include <GL/glut.h>
 
+void init();
+void lineSegment();
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(50,100);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("AN Example OpenGL Program");
+    init();
+    glutDisplayFunc(lineSegment);
+    glutMainLoop();
+    return 0;
+}
+
+void init()
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0,200.0,0.0,150.0);
+}
+
+void lineSegment()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0, 0.4, 0.2);
+    glBegin(GL_LINES);
+        glVertex2i(180,15);
+        glVertex2i(10,145);
+    glEnd();
+    glFlush();
+}
